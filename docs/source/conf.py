5ecbed57b6d3e1b3f63d70be8d8a948c32b3020f
Tu Hu <tuhu@ssi.dk>
2025-02-20 11:31:26 +0100

    Update documentation to reflect project name change from Lumache to Scanpy Tools

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'scanpytools'
+copyright = '2025, Statens Serum Institut'
+author = 'Tu Hu'
 
 release = '0.1'
 version = '0.1.0'
